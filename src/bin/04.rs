c3b02c53d105865c71c77cacb9287a5460d1ee78
froith <felix.roithmayr@aon.at>
2025-12-18 10:39:36 +0100

    speedup attempt with ndarray for day 4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,103 +1,78 @@
-use std::collections::HashMap;
+use ndarray::{Array2, arr2, s};
+use std::collections::HashSet;
 
 advent_of_code::solution!(4);
 
-#[derive(Eq, PartialEq, Clone)]
-enum RollType {
-    Roll,
-    NoRoll,
-    ClearedRoll,
-}
-
-impl From<char> for RollType {
-    fn from(c: char) -> Self {
-        match c {
-            '@' => RollType::Roll,
-            '.' => RollType::NoRoll,
-            'x' => RollType::ClearedRoll,
-            _ => panic!("Invalid char"),
-        }
-    }
-}
-
 #[derive(Clone)]
 struct RollMap {
-    map: HashMap<(isize, isize), RollType>,
+    h: usize,
+    w: usize,
+    rolls: HashSet<(usize, usize)>,
+    map: Array2<u8>,
 }
 
 impl RollMap {
     fn new(input: &str) -> Self {
-        let mut map: HashMap<(isize, isize), RollType> = HashMap::new();
+        let mut rolls: HashSet<(usize, usize)> = HashSet::new();
         let lines: Vec<&str> = input.trim().lines().map(|l| l.trim()).collect();
-        let h = lines.len();
-        let w = lines[0].len();
-        for y in 0..h {
-            let cs = lines[y].chars().collect::<Vec<char>>();
-            for x in 0..w {
-                map.insert((x as isize, y as isize), cs[x].into());
-            }
-        }
-        RollMap { map }
-    }
+        let h: usize = lines.len();
+        let w: usize = lines[0].len();
+        let mut map = Array2::from_elem((h, w), 0);
+        for (y, line) in lines.iter().enumerate() {
+            for (x, c) in line.chars().enumerate() {
+                if c == '@' {
+                    rolls.insert((x, y));
 
-    fn get_num_neighbors(&self, x: isize, y: isize) -> usize {
-        let mut num = 0;
-        for dx in -1..=1 {
-            for dy in -1..=1 {
-                if dx == 0 && dy == 0 {
-                    continue;
-                }
-                let nx = x + dx;
-                let ny = y + dy;
-                if nx < 0 || ny < 0 {
-                    continue;
-                }
-                if let Some(nt) = self.map.get(&(nx, ny))
-                    && *nt == RollType::Roll
-                {
-                    num += 1;
+                    let mut sl = map.slice_mut(s![
+                        y.saturating_sub(1)..=(y + 1).min(h - 1),
+                        x.saturating_sub(1)..=(x + 1).min(w - 1)
+                    ]);
+                    sl.iter_mut().for_each(|v| *v += 1);
                 }
             }
         }
-        num
+        RollMap { h, w, rolls, map }
     }
 
-    fn clear_rolls(&mut self, max_neighbors: usize) {
-        let orig_map = self.clone();
-        self.map.iter_mut().for_each(|((x, y), v)| {
-            if *v == RollType::Roll && orig_map.get_num_neighbors(*x, *y) < max_neighbors {
-                *v = RollType::ClearedRoll;
-            }
-        });
-    }
-
-    fn count_cleared(&self) -> u64 {
-        self.map
+    fn clear_rolls(&mut self, max_neighbors: u8) -> usize {
+        let l = self.rolls.len();
+        let orig_map = self.map.clone();
+        self.rolls = self
+            .rolls
             .iter()
-            .filter(|(_, v)| **v == RollType::ClearedRoll)
-            .count() as u64
+            .filter_map(|&(x, y)| {
+                if *orig_map.get((y, x)).unwrap() > max_neighbors {
+                    Some((x, y))
+                } else {
+                    let mut sl = self.map.slice_mut(s![
+                        y.saturating_sub(1)..=(y + 1).min(self.h - 1),
+                        x.saturating_sub(1)..=(x + 1).min(self.w - 1)
+                    ]);
+                    sl.iter_mut().for_each(|v| *v -= 1);
+                    None
+                }
+            })
+            .collect();
+        l - self.rolls.len()
     }
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
     let mut roll_map = RollMap::new(input);
-    roll_map.clear_rolls(4);
-    Some(roll_map.count_cleared())
+    Some(roll_map.clear_rolls(4) as u64)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
     let mut roll_map = RollMap::new(input);
-    roll_map.clear_rolls(4);
-    let mut nc = roll_map.count_cleared();
+    let mut nc = roll_map.clear_rolls(4);
     loop {
-        roll_map.clear_rolls(4);
-        let nc_new = roll_map.count_cleared();
-        if nc_new == nc {
+        let nc_new = roll_map.clear_rolls(4);
+        if nc_new == 0 {
             break;
         }
-        nc = nc_new;
+        nc += nc_new;
     }
-    Some(nc)
+    Some(nc as u64)
 }
 
 #[cfg(test)]
@@ -108,11 +83,15 @@ mod tests {
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(13));
+        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(1505));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(43));
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(9182));
     }
 }
