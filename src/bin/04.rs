8c009777dfa8e378a413ed50dbe5ec482bf49867
froith <felix.roithmayr@aon.at>
2025-12-18 12:27:00 +0100

    further optimized day4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,18 +1,16 @@
-use std::collections::HashSet;
-
 advent_of_code::solution!(4);
 
 #[derive(Clone)]
 struct RollMap {
     h: usize,
     w: usize,
-    rolls: HashSet<(usize, usize)>,
+    rolls: Vec<(usize, usize)>,
     map: Vec<Vec<u8>>,
 }
 
 impl RollMap {
     fn new(input: &str) -> Self {
-        let mut rolls: HashSet<(usize, usize)> = HashSet::new();
+        let mut rolls: Vec<(usize, usize)> = Vec::new();
         let lines: Vec<&str> = input.trim().lines().map(|l| l.trim()).collect();
         let h: usize = lines.len();
         let w: usize = lines[0].len();
@@ -20,7 +18,7 @@ impl RollMap {
         for (y, line) in lines.iter().enumerate() {
             for (x, c) in line.chars().enumerate() {
                 if c == '@' {
-                    rolls.insert((x, y));
+                    rolls.push((x, y));
 
                     map[y.saturating_sub(1)..=(y + 1).min(h - 1)]
                         .iter_mut()
@@ -33,6 +31,13 @@ impl RollMap {
             }
         }
         RollMap { h, w, rolls, map }
+    }
+
+    fn get_clearable(&self, max_neighbors: u8) -> usize {
+        self.rolls
+            .iter()
+            .filter(|&&(x, y)| self.map[y][x] <= max_neighbors)
+            .count()
     }
 
     fn clear_rolls(&mut self, max_neighbors: u8) -> usize {
@@ -60,8 +65,8 @@ impl RollMap {
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let mut roll_map = RollMap::new(input);
-    Some(roll_map.clear_rolls(4) as u64)
+    let roll_map = RollMap::new(input);
+    Some(roll_map.get_clearable(4) as u64)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
@@ -85,15 +90,11 @@ mod tests {
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(13));
-        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
-        assert_eq!(result, Some(1505));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(43));
-        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
-        assert_eq!(result, Some(9182));
     }
 }
